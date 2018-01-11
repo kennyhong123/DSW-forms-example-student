7e7f71aed7469d515caf0254c2eb93d933168325
kennyhong123 <31251440+kennyhong123@users.noreply.github.com>
2018-01-11 10:12:00 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,7 +6,7 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
 
-@app.route("/response" methods=["POST"])
+@app.route("/response", methods=["POST"])
 def render_response():
     color=request.forms['color']
     if color == 'pink':
